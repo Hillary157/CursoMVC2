a8e637bebf6dbb62f73c9c8f9012ed456d46db17
Hillary157 <thaina.9016@gmail.com>
2021-09-15 14:00:56 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVC2/Models/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC2.Models
+{
+    public class Categoria
+    {
+        public int id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage ="O campo descrição é obrigatório")]
+
+        public string Descricao { get; set; }
+        [Range(1,10,ErrorMessage ="Valor fora da faixa")]
+
+        public List <Produto> Produtos { get; set; }
+    }
+}
+
+
